e0b905d6059f152d069d27cf8c52664cbdf06465
Eng-Mohamed-Raya <mohamedyasserraya@gmail.com>
2025-08-05 13:12:23 +0300

    Day 3 (day 1 js )

---
+++ Day3/js/main.js
@@ -0,0 +1,60 @@
+console.log("-".repeat(30))
+
+//** task 2.1 */
+
+var myNum= 10
+var myStr="I am a string"
+var unDefined;
+var nullType=null;
+var myBoolVar=true;
+
+console.log(`type of myNum: ${typeof myNum}`)
+console.log(`type of myStr: ${typeof myStr}`)
+console.log(`type of unDefined: ${typeof unDefined}`)
+console.log(`type of nullType: ${typeof nullType}`)
+console.log(`type of myBoolVar: ${typeof myBoolVar}`)
+
+console.log("-".repeat(30))
+
+//** task 2.2 */
+
+var userName="Mohamed Yasser Raya";
+var userAge="20";
+
+console.log(`Hello ${userName}, you are ${userAge} years old.`)
+
+console.log("-".repeat(30))
+
+//** task 3 */
+
+var x = 10, y = 5, z = 2;
+
+console.log(`x + y = ${x+y}`)
+console.log(`x - z = ${x-z}`)
+console.log(`x * y = ${x*y}`)
+console.log(`x / z = ${x/z}`)
+console.log(`x % z = ${x % z}`)
+console.log(`++x = ${++x}`)
+console.log(`--y = ${--y}`)
+
+console.log("-".repeat(30))
+
+//** task 4 */
+
+var score=0;
+
+console.log(`score+5 = ${score+=5}`)
+console.log(`score-2 = ${score-=2}`)
+console.log(`score*3 = ${score*=3}`)
+console.log(`score/2 = ${score/=2}`)
+
+console.log("-".repeat(30))
+
+//** task 5 */
+
+var isLoggedIn =true;
+
+console.log(isLoggedIn && "Hello World!")
+console.log(!isLoggedIn && "Hello World!")
+
+
