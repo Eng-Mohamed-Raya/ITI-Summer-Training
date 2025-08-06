c4927e13f42a1086db4d7aef25bfa30bc03a4ff9
Eng-Mohamed-Raya <mohamedyasserraya@gmail.com>
2025-08-06 13:56:04 +0300

    Day 4 ( day 2 JS)

---
+++ Day4/js/main.js
@@ -0,0 +1,57 @@
+//**  task 1 
+
+
+// var sum=0,input="";
+// var isValid=true;
+// do{
+//     input=Number(prompt("Enter you value do you need sum : "));
+//    if(!isNaN(input)){
+//        sum+=input ;  
+//        if(sum>=100 )
+//          isValid=false;
+//    }
+// }while(isValid || input==0);
+// console.log(sum)
+
+//**  task 2
+
+//  var name="",color="";
+//  alert("Welcome in my website!")
+// do{
+//      name=prompt("Enter your name");
+// }while(  !isNaN(name) || name==="" || name ==="null") 
+// do{
+//      color=prompt("Enter your favorite color");
+   
+// }while( !isNaN(color) || color==="" || color ==="null");
+// switch(color){
+//     case "red":
+//         break;
+//     case "green":
+//         break;
+//     case "blue":
+//         break;
+//     default :
+//         color="black";
+// }
+
+// alert(`Welcome ${name} , your favorite color is ${color}`);
+// document.writeln(`<P style="color:${color}">Welcome ${name} , your favorite color is ${color}</p>`)
+
+
+
+//**  task 3
+
+
+// var msg="";
+// do{
+//      msg=prompt("Enter your msg");
+// }while(  !isNaN(msg) || msg==="" || msg ==="null") 
+
+
+// for (var i = 1; i <=6; i++) {
+    
+//     document.writeln(`<h${i}>${msg}</h${i}>`)
+// }
+
+
